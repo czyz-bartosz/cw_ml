f2eee06b27b7ffdac8540475911f64acf22ab2eb
Bartosz Czyż <88885276+czyz-bartosz@users.noreply.github.com>
2025-05-27 18:45:39 +0200

    add plot

--- function.R
+++ function.R
@@ -1,9 +1,36 @@
-# funkcja
+library(ggplot2)
+
+# funkcja liczy parametr y na podstawie zbioru danych x
+# x - zbiór danych w postaci wektora liczbowego 
+# funkcja sprawdza, czy zmienna jest typu numerycznego 
 
 new_f <- function(x){
   
-  y <- x / 100 + sqrt(x)
+  if (!is.numeric(x)) { stop("Obiekt wejściowy x nie jest typu numeric") }
+  
+  srednia <- mean(x)
+  
+  y <- srednia / 10 + sqrt(srednia)
   
   return(y)
   
 }
+
+# test 1 funkcji 
+
+dane <- 1:10
+dane
+new_f(x = dane)
+
+ggplot(data.frame(x = 1:10), aes(x = x)) +
+  stat_function(fun = new_f, color = "blue") +
+  labs(title = "Plot of f(x)",
+       x = "x",
+       y = "f(x)") +
+  theme_minimal()
+
+# test 2 - sprawdzamy, czy odporna
+
+new_f(x = letters)
+
+letters |> str()
